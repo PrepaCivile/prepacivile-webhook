9b62678515146b969067b3966dc5357476f66c85
PrepaCivile <mathieumontaroux@gmail.com>
2025-06-02 19:58:00 -0400

    🔥 Corrige le parsing de la private key (sans \\n)

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FB_PROJECT_ID,
     clientEmail: process.env.FB_CLIENT_EMAIL,
-    privateKey: process.env.FB_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    privateKey: process.env.FB_PRIVATE_KEY,
   }),
 });
 const db = admin.firestore();
